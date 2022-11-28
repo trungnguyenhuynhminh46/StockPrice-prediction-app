8f9e80a495a3b64b453518eca68870fdd549bb03
trungnguyenhuynhminh46 <trungnguyenhuynhminh46@gmail.com>
2022-11-28 20:57:45 +0700

    Optimize code

--- app.py
+++ app.py
@@ -7,27 +7,17 @@ from keras.models import load_model
 import streamlit as st
 from sklearn.preprocessing import MinMaxScaler
 
-# print("numpy==", np.__version__)
-# print("pandas==", pd.__version__)
-# import matplotlib
-# print("matplotlib==", matplotlib.__version__)
-# print("pandas_datareader==", data.__version__)
-# import keras
-# print("keras==", keras.__version__)
-# print("streamlit==", st.__version__)
-# import sklearn
-# print("sklearn==", sklearn.__version__)
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+import math
 
-# Funcitons
+# Functions
 def createDataset(dataset, look_back):
     dataX, dataY = [], []
     for i in range(len(dataset)-look_back-1):
         dataX.append(dataset[i:(i+look_back), 0])
         dataY.append(dataset[i+look_back, 0])
     return np.array(dataX), np.array(dataY)
-# Dữ liệu người dùng nhập từ giao diện
-# start = "2005-01-01"
-# end = "2019-12-31"
 
 st.write(
     """
@@ -40,7 +30,7 @@ model_name = st.selectbox(
         "Chọn mô hình mà bạn muốn dùng để dự đoán",
         ("FFNN", "RNN", "LSTM", "GRU")
     )
-user_input = st.text_input("Nhập vào mã chứng khoán muốn dự đoán", "AAPL")
+stock_code = st.text_input("Nhập vào mã chứng khoán muốn dự đoán", "AAPL")
 start = st.date_input(
     "Nhập vào ngày bắt đầu dự đoán",
     datetime.date(2005, 1, 1))
@@ -49,27 +39,25 @@ end = st.date_input(
     datetime.date(2019, 12, 31))
 
 # Dữ liệu lấy từ DataReader
-raw_data = data.DataReader(user_input, 'yahoo', start, end)
+raw_data = data.DataReader(stock_code, 'yahoo', start, end)
 df = raw_data.dropna();
 
 #Mô tả dữ liệu
-st.subheader("Thông tin của mã chứng khoán {stock_code} từ {start_date} đến {end_date}".format(stock_code = user_input, start_date = start, end_date = end))
+st.subheader("Thông tin của mã chứng khoán {stock_code} từ {start_date} đến {end_date}".format(stock_code = stock_code, start_date = start, end_date = end))
 st.write(df.describe())
 
 #Trực hóa dữ liệu
-st.subheader("Xu hướng chuyển dịch giá đóng của mã {stock_code} từ {start_date} đến {end_date}".format(stock_code=user_input, start_date = start, end_date = end))
-ma100 = df.Close.rolling(100).mean()
-fig = plt.figure(figsize=(12, 6))
-plt.plot(df.Close)
-st.pyplot(fig)
 
-st.subheader("Xu hướng chuyển dịch giá đóng cùng với các đường MA100 và MA200 của mã {stock_code} trong khoảng đầu năm 2005 đến cuối năm 2019".format(stock_code=user_input))
+st.subheader("Xu hướng chuyển dịch giá đóng cùng với các đường MA100 và MA200 của mã {stock_code} từ {start_date} đến {end_date}".format(stock_code=stock_code,start_date = start, end_date = end))
 ma100 = df.Close.rolling(100).mean()
 ma200 = df.Close.rolling(200).mean()
 fig = plt.figure(figsize=(12, 6))
-plt.plot(ma100, 'r')
-plt.plot(ma200, 'g')
-plt.plot(df.Close, 'b')
+plt.plot(ma100, 'r', label="MA100", color="r")
+plt.plot(ma200, 'g', label="MA200", color="b")
+plt.plot(df.Close, 'b', label="Actual Closing Prices", color="g")
+legend = plt.legend(loc="best", fontsize = 12,frameon = True)
+legend.get_frame().set_edgecolor('b')
+legend.get_frame().set_linewidth(0.4)
 st.pyplot(fig)
 
 # Xử lý dữ liệu
@@ -78,15 +66,9 @@ dataset = df[['Close']].values.astype('float32');
 scaler = MinMaxScaler(feature_range=(0, 1))
 dataset = scaler.fit_transform(dataset)
 
-# Tập test và tập dự đoán được chia theo tỷ lệ 8 - 2 
-train_size = int(len(dataset) * 0.80)
-test_size = len(dataset) - train_size
-
-train_ds, test_ds = dataset[0:train_size,:], dataset[train_size:len(dataset)+1,:]
-
 look_back = 10
-trainX, trainY = createDataset(train_ds, look_back)
-testX, testY = createDataset(test_ds, look_back)
+
+X, Y = createDataset(dataset, look_back)
 
 #Load model
 models_dir = "./h5/"
@@ -98,36 +80,49 @@ model_names_map = {
 }
 model = load_model(models_dir + model_names_map[model_name])
 
-# Dùng model để dự đoán
-trainPredict = model.predict(trainX)
-testPredict = model.predict(testX)
-
-trainPredict = scaler.inverse_transform(trainPredict)
-trainY = scaler.inverse_transform([trainY])
-testPredict = scaler.inverse_transform(testPredict)
-testY = scaler.inverse_transform([testY])
+predict = model.predict(X)
+predict = scaler.inverse_transform(predict)
+Y = scaler.inverse_transform([Y])
 
 # Plotting
-trainPredictPlot = np.empty_like(dataset)
-trainPredictPlot[:, :] = np.nan
-trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+predictPlot = np.empty_like(dataset)
+predictPlot[:, :] = np.nan
+predictPlot[look_back:len(predict)+look_back, :] = predict
 
-testPredictPlot = np.empty_like(dataset)
-testPredictPlot[:, :] = np.nan
-testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
-
-st.subheader("Kết quả dự giá giá chứng khoáng của mã {stock_code} trên tập train và tập test so với kết quả thực tế sử dụng model {model_name} là".format(stock_code=user_input, model_name = model_name))
-
+st.subheader("Kết quả dự giá giá chứng khoáng của mã {stock_code} trên tập train và tập test so với kết quả thực tế sử dụng model {model_name} là".format(stock_code=stock_code, model_name = model_name))
 plt.style.use('ggplot')
 fig = plt.figure(figsize=(12,6), dpi=110)
-plt.grid(color='grey', linestyle='dashed')
 plt.xlabel('Observations')
-plt.ylabel(user_input,rotation=90)
+plt.ylabel(stock_code,rotation=90)
 plt.plot(scaler.inverse_transform(dataset), label = 'Actual Closing Prices', linewidth = 1.2, color = 'c')
-plt.plot(trainPredictPlot, label = 'A.I. Train Data Price Predictions_After fit', linewidth = 0.9, color = 'k')
-plt.plot(testPredictPlot, label = 'A.I. Test Data Price Predictions', linewidth = 0.9, color = 'r')
+plt.plot(predictPlot, label = 'Predicted Closing Price', linewidth = 0.9, color = 'k')
 legend = plt.legend(fontsize = 12,frameon = True)
 legend.get_frame().set_edgecolor('b')
 legend.get_frame().set_linewidth(0.4)
 
 st.pyplot(fig)
+
+# Get accuracies
+def mean_absolute_percentage_error(y_true, y_pred): 
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+def root_mean_squared_error(y_true, y_pred):
+    mse = mean_squared_error(y_true, y_pred)
+    rmse = np.sqrt(mse)
+    return rmse
+
+def calculate_performance(y_true, y_pred):
+    mse = mean_squared_error(y_true, y_pred)
+    mae = mean_absolute_error(y_true, y_pred)
+    mape = mean_absolute_percentage_error(y_true, y_pred)
+    rmse = root_mean_squared_error(y_true, y_pred)
+    return round(mse, 3), round(mae, 3), round(mape, 3), round(rmse, 3)
+
+mse, mae, mape, rmse = calculate_performance(Y[0],predict[:, 0])
+
+st.subheader("Độ chính xác mô hình dự đoán của mã {stock_code} từ {start_date} đến {end_date}".format(stock_code=stock_code,start_date = start, end_date = end))
+d = {'RMSE': [rmse], 'MSE': [mse], 'MAE': [mae], "MAPE": [mape]}
+df = pd.DataFrame(data=d, index=[model_name])
+
+st.dataframe(df)  # Same as st.write(df)
